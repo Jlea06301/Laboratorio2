0944838d964136a4758cacc310f772a82611503d
Jlea06301 <jorge06301@gmail.com>
2023-11-15 23:32:17 -0500

    commit 1

---
+++ src/main/java/com/arquitectura/crudinstitucion/CrudInstitucionApplication.java
@@ -0,0 +1,13 @@
+package com.arquitectura.crudinstitucion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudInstitucionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudInstitucionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arquitectura/crudinstitucion/CrudInstitucionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arquitectura.crudinstitucion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudInstitucionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
